2a97801340e774375810ec8fbdfc7a9d70e4dbf9
probuse <nprobuse@gmail.com>
2017-12-05 18:31:13 +0300

    added constructor and modifier functions

--- scrapBook/HelloWorldContract.sol
+++ scrapBook/HelloWorldContract.sol
@@ -3,13 +3,26 @@ pragma solidity ^0.4.0;
 
 contract HelloWorldContract {
     string word = "Hello World";
+    address issuer;
+
+    function HelloWorldContract(){
+        issuer = msg.sender;
+    }
+
+    modifier ifIssuer() {
+        if (issuer != msg.sender) {
+            throw;
+        }else {
+            _;
+        }
+    }
 
     function getWord() constant returns(string) {
         return word;
     }
 
-    function setWord(string newWord) returns(string) {
+    function setWord(string newWord) ifIssuer returns(string) {
         word = newWord;
-        return word;
+        return "This is the creator";
     }
 }
